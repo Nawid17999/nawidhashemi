bce2a7beaeba516415b46652c0743f4bdb9b74b3
Nawid17999 <nawid.hashemi@dcmail.ca>
2024-11-20 14:13:11 -0500

    added
    new content and changes to work

--- Lab4/part1/main.js
+++ Lab4/part1/main.js
@@ -1,35 +1,36 @@
+// Allow user to set a custom name + story 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
-
+// Random value chosen
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
-
+// Text of story
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
-
+/* Create story */
 function result() {
   let newStory = storyText;
-
+/* Replace placeholders with randomly selected values */
   const xItem = randomValueFromArray(insertX);
   const yItem = randomValueFromArray(insertY);
   const zItem = randomValueFromArray(insertZ);
-
+/* Replace all occurrences of placeholders in the story */
   newStory = newStory.replaceAll(':insertx:',xItem);
   newStory = newStory.replaceAll(':inserty:',yItem);
   newStory = newStory.replaceAll(':insertz:',zItem);
-
+/* If the user enters a custom name, replace 'Bob' with the custom name */
   if (customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replaceAll('Bob', name);
   }
-
+/* Check if the UK metric checkbox is checked to adjust units */
   if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
